65fa2955d7361bb590d4a1da79142f3481cfdc1e
nanter1986 <nanter1986@gmail.com>
2021-04-18 14:53:24 +0300

    enemy animation tree

--- Enemy.gd
+++ Enemy.gd
@@ -6,7 +6,9 @@ export(int) var ACCELERATION=200
 export(float) var scale_factor=0.5
 
 onready var animation_player=$AnimationPlayer
+onready var animation_tree=$AnimationTree
 var velocity=Vector2()
+onready var state_machine
 enum direction{
 	LEFT,
 	RIGHT
@@ -22,6 +24,7 @@ var action_state
 var direction_state
 
 func _ready():
+	state_machine= animation_tree.get("parameters/playback")
 	direction_state=direction.LEFT
 	action_state=action.IDLE
 	self.scale.x*=scale_factor
@@ -39,14 +42,18 @@ func _physics_process(delta):
 func get_random_vector():
 	return Vector2(rand_range(-1, 1), rand_range(-1, 1))
 
+func set_animation_tree_parameters(input_vector):
+	animation_tree.set("parameters/Idle/blend_position",input_vector)
+	animation_tree.set("parameters/Move/blend_position",input_vector)
+
 func move_state(delta):
 	var input_vector=get_random_vector()
 	print(str(input_vector))
 	if input_vector != Vector2.ZERO:
-		#set_animation_tree_parameters(input_vector)
-		#state_machine.travel("Move")
+		set_animation_tree_parameters(input_vector)
+		state_machine.travel("move")
 		velocity=velocity.move_toward(input_vector*SPEED,delta*ACCELERATION)
 	else:
-		#state_machine.travel("Idle")
+		state_machine.travel("idle")
 		velocity=velocity.move_toward(Vector2.ZERO,FRICTION*delta)
 	velocity = move_and_slide(velocity)
