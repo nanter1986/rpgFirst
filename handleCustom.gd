ce35b52cf5f5a8d01769b21d85b458ed6f9664b9
nanter1986 <nanter1986@gmail.com>
2021-05-08 21:41:45 +0300

    debugging controller

--- handleCustom.gd
+++ handleCustom.gd
@@ -1,13 +1,17 @@
 extends TouchScreenButton
 
 var radius=Vector2(150,150)
+var positionCurrent=Vector2(0,0)
+
+func _physics_process(delta):
+	position=positionCurrent
 
 func _input(event):
 	if event.is_pressed():
 		if event.position>radius:
 			pass
 		else:
-			position=event.position
+			positionCurrent=event.position
 		
 		print(event.position)
 
